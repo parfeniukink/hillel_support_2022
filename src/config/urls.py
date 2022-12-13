82d105aa8fc2c0fbc9306cafbd88a44c9dc0035f
Dmytro Parfeniuk <parfeniukink@gmail.com>
2022-12-13 22:16:42 +0200

    John simple API implementation

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,21 +1,22 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+from dataclasses import dataclass, asdict
 from django.urls import path
+from django.http import HttpResponse
+import json
+
+
+@dataclass
+class Person:
+    name: str
+    age: int
+
+
+def get_john(variable):
+    john = Person(name="John", age=30)
+    content = json.dumps(asdict(john))
+    content_type = "application/json"
+    return HttpResponse(content, content_type=content_type)
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("john/", get_john),
 ]
